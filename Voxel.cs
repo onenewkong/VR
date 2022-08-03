593699086831aadf28eb56514311ec0e6fd9f623
onenewkong <hub1375@gmail.com>
2022-08-03 16:48:49 +0900

    인생 유니티 VR/AR 교과서 - 1.1.1 ~ 1.1.6 카메라 컨트롤, 바닥 만들기, 복셀 만들기, 복셀 제작자 만들기

---
+++ Voxel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voxel : MonoBehaviour
+{
+    // 1. 복셀이 날아갈 속도 속성
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 2. 랜덤한 방향을 찾음
+        Vector3 direction = Random.insideUnitSphere;
+        // 3. 랜덤한 방향으로 날아가는 속도를 줌
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = direction * speed;
+    }
+
+    // 복셀을 제거할 시간
+    public float destoryTime = 3.0f;
+    // 경과 시간
+    float currentTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. 시간이 흘러야 함
+        currentTime += Time.deltaTime;
+        // 2. 제거 시간이 됐으니까
+        // 만약 경과 시간이 제거 시간을 초과했다면
+        if(currentTime > destoryTime)
+        {
+            // 3. 복셀을 제거하고 싶음
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ VoxelMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMaker : MonoBehaviour
+{
+    // 복셀 공장
+    public GameObject voxelFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 사용자가 마우스를 클릭한 지점에 복셀을 1개 만들고 싶음
+
+        // 1. 사용자가 마우스를 클릭했다면
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // 2. 마우스의 위치가 바닥 위에 위치해 있다면
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                // 3. 복셀 공장에서 복셀을 만들어야 함
+                GameObject voxel = Instantiate(voxelFactory);
+                // 4. 복셀을 배치하고 싶음
+                voxel.transform.position = hitInfo.point;
+            }
+        }
+    }
+}
